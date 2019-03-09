76630daf6dffd8daf95f4ef8670efc35da3bf895
Seth Hendrick <seth@shendrick.net>
2019-03-08 22:40:42 -0500

    Can add text via a button.

--- AssetManager.Web/Scripts/AssetTypeMaker.ts
+++ AssetManager.Web/Scripts/AssetTypeMaker.ts
@@ -1,5 +1,29 @@
-﻿function sayHello() {
-    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
-    return `Hello from ${compiler} and ${framework}!`;
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+class AssetTypeMaker {
+
+    // ---------------- Fields ----------------
+
+    private readonly appDiv: HTMLDivElement;
+
+    private htmlString: string
+
+    // ---------------- Constructor ----------------
+
+    constructor() {
+        this.appDiv = document.getElementById("app") as HTMLDivElement;
+        this.htmlString = "";
+    }
+
+    // ---------------- Functions ----------------
+
+    public AddAttribute(attr: string): void {
+        this.htmlString += "<li>" + attr + "</li>";
+        this.appDiv.innerHTML = "<ul>" + this.htmlString + "</ul>"
+    }
 }
