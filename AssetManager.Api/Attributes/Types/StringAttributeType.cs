2320762611b20107dec011e186312cb1decd7c65
Seth Hendrick <seth@shendrick.net>
2019-03-16 14:53:09 -0400

    Rename Attributes -> AttributeTypes.

---
+++ AssetManager.Api/Attributes/Types/StringAttributeType.cs
@@ -0,0 +1,17 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetManager.Api.Attributes.Types
+{
+    public class StringAttributeType
+    {
+    }
+}
