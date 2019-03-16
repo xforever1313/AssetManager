ddb5b555067ba7c4e6cf9d6910c716bccc7de4a6
Seth Hendrick <seth@shendrick.net>
2019-03-16 16:24:44 -0400

    Added remaining existing attribute types.

---
+++ AssetManager.Api/Attributes/Types/IntegerAttributeType.cs
@@ -0,0 +1,28 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+namespace AssetManager.Api.Attributes.Types
+{
+    public class IntegerAttributeType : BaseAttributeType
+    {
+        // ---------------- Constructor ----------------
+
+        public IntegerAttributeType() :
+            base()
+        {
+        }
+
+        // ---------------- Functions ----------------
+
+        protected override bool ValidateInternal( out string errors )
+        {
+            // Nothing to validate... Yet...
+            errors = string.Empty;
+            return true;
+        }
+    }
+}
