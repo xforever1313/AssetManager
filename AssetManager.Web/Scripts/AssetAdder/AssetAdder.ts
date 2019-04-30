2eb60476eff03d75517243f9e33bb3ee264fcf96
Seth Hendrick <seth@shendrick.net>
2019-04-29 20:07:07 -0400

    Added stubs for the AssetAdder HTML view.

---
+++ AssetManager.Web/Scripts/AssetAdder/AssetAdder.ts
@@ -0,0 +1,27 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+class AssetAdder {
+
+    // ---------------- Fields ----------------
+
+    readonly attributes: Array<IAttribute>;
+
+    // ---------------- Constructor ----------------
+
+    constructor() {
+        this.attributes = new Array<IAttribute>();
+    }
+
+    // ---------------- Properties ----------------
+
+    // ---------------- Functions ----------------
+
+    public AddAttribute(attribute: IAttribute) {
+        this.attributes.push(attribute);
+    }
+}
---
+++ AssetManager.Web/Scripts/AssetAdder/IAttribute.ts
@@ -0,0 +1,22 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+interface IAttribute {
+
+    // ---------------- Functions -----------------
+
+    EnableForm(): void;
+
+    DisableForm(): void;
+
+    /**
+     * Ensures the attribute is in a valid state.
+     **/
+    Validate(): boolean;
+
+    ToJson(): object;
+}
