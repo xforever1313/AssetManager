1304dda5c20f1b3423103564b38cf12c02bdfcb1
Seth Hendrick <seth@shendrick.net>
2019-05-05 18:38:40 -0400

    Can add attributes to the AssetAdder typescript function.

---
+++ AssetManager.Web/IAttributeTypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using AssetManager.Api.Attributes.Types;
+using SethCS.Extensions;
+
+namespace AssetManager.Web
+{
+    public static class IAttributeTypeExtensions
+    {
+        public static string EscapeKey(this IAttributeType type)
+        {
+            return type.Key.NormalizeWhiteSpace( '_' );
+        }
+    }
+}
