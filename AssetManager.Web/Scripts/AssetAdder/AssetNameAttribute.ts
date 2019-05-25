bce5bc3e80636bbde51e6dadee6c643171c18924
Seth Hendrick <seth@shendrick.net>
2019-05-25 15:42:01 -0400

    Can now add the asset name when adding an asset.

---
+++ AssetManager.Web/Scripts/AssetAdder/AssetNameAttribute.ts
@@ -0,0 +1,59 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+class AssetNameAttribute extends BaseAttribute {
+
+    // ---------------- Fields ----------------
+
+    private value: string;
+
+    // ---------------- Constructor ----------------
+
+    constructor(info: AssetNameAttributeTypeInfo, key: string) {
+        super(info.AttributeType, key)
+    }
+
+    // ---------------- Functions ----------------
+
+    public Validate(): Array<string> {
+        let errors = new Array<string>();
+
+        if (Helpers.StringIsNullOrEmpty(this.value)) {
+            errors.push("No value specified.");
+        }
+
+        return errors;
+    }
+
+    public ToJson(): object {
+        let data = {
+            "Key": this.Key,
+            "AttributeType": this.AttributeType,
+            "Value": this.GetValue()
+        };
+
+        return data;
+    }
+
+    // -------- Setters --------
+
+    public SetValue(newValue: string): AssetNameAttribute {
+        if (newValue === "") {
+            this.value = null;
+        }
+        else {
+            this.value = newValue;
+        }
+        return this;
+    }
+
+    // -------- Getters --------
+
+    public GetValue(): string {
+        return this.value;
+    }
+}
---
+++ AssetManager.Web/Scripts/Common/AssetNameAttributeTypeInfo.ts
@@ -0,0 +1,26 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+class AssetNameAttributeTypeInfo {
+
+    // ---------------- Constructor ----------------
+
+    constructor() {
+        this.AttributeType = AttributeType.AssetNameAttribute;
+    }
+
+    // ---------------- Properties ----------------
+
+    public readonly AttributeType: AttributeType;
+
+    // ---------------- Functions ----------------
+
+    public Validate(): Array<string> {
+        // Nothing to validate.
+        return null;
+    }
+}
