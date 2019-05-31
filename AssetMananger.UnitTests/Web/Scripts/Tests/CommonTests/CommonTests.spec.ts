2c278acf101d561fa5c44f03485a3621360036a1
Seth Hendrick <seth@shendrick.net>
2019-05-31 17:42:22 -0400

    Added the ability to run typescript tests via NUnit by calling a subprocess.

---
+++ AssetMananger.UnitTests/Web/Scripts/Tests/CommonTests/CommonTests.spec.ts
@@ -0,0 +1,8 @@
+﻿import { expect } from 'chai';
+import 'mocha';
+
+describe('Hello', function () {
+    it('Test 1', function () {
+        expect('hello').to.equal('hello');
+    })
+});
---
+++ AssetMananger.UnitTests/Web/Scripts/TypeScriptTests.cs
@@ -0,0 +1,87 @@
+﻿//
+//          Copyright Seth Hendrick 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+
+namespace AssetMananger.UnitTests.Web.Scripts
+{
+    [TestFixture]
+    public class TypeScriptTests
+    {
+        // ---------------- Fields ----------------
+
+        string testDirectory;
+        string nodeModules;
+
+        // ---------------- Setup / Teardown ----------------
+
+        [OneTimeSetUp]
+        public void FixtureSetup()
+        {
+            this.testDirectory = Path.Combine(
+                TestContext.CurrentContext.TestDirectory,
+                "..",
+                "..",
+                "..",
+                "Web",
+                "Scripts"
+            );
+            this.nodeModules = Path.Combine( this.testDirectory, "node_modules" );
+
+            if ( Directory.Exists( this.nodeModules ) == false )
+            {
+                this.RunSubProcess( "npm", "install", this.testDirectory );
+            }
+        }
+
+        [OneTimeTearDown]
+        public void FixtureTeardown()
+        {
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+        }
+
+        [TearDown]
+        public void TestTeardown()
+        {
+        }
+
+        // ---------------- Tests ----------------
+
+        [Test]
+        public void TestAll()
+        {
+            this.RunSubProcess( "npm", "test", this.testDirectory );
+        }
+
+        // ---------------- Test Helpers ----------------
+
+        private void RunSubProcess( string command, string arguments, string workingDirectory )
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo( command, arguments )
+            {
+                UseShellExecute = true,
+                WorkingDirectory = workingDirectory
+            };
+
+            using ( Process process = new Process() )
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+
+                Assert.AreEqual( 0, process.ExitCode );
+            }
+        }
+    }
+}
